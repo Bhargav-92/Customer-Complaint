667bdd757aa13d29f1cd1102b38c90079ab6e430
Bhargav Thakar <tkr.bhargav92@gmail.com>
2024-05-17 14:20:54 +0530

    add console

--- Backend/Controllers/complaint.js
+++ Backend/Controllers/complaint.js
@@ -38,6 +38,8 @@ exports.updateComplaintStatus = async (req, res) => {
             { new: true, runValidators: true }
         );
 
+
+        console.log(updatedComplaint)
         if (!updatedComplaint) {
             return res.status(404).json({ message: "Complaint not found" });
         }
