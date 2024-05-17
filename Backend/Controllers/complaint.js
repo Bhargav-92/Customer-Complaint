535715c354606d25c4a93226a20c55ac0171cd30
Aarsh Shukla <105079929+aarsh2982@users.noreply.github.com>
2024-05-17 14:27:03 +0530

    First Commit

--- Backend/Controllers/complaint.js
+++ Backend/Controllers/complaint.js
@@ -38,6 +38,7 @@ exports.updateComplaintStatus = async (req, res) => {
             { new: true, runValidators: true }
         );
 
+        console.log("Test Push");
 
         if (!updatedComplaint) {
             return res.status(404).json({ message: "Complaint not found" });
