e8eb606ceaa90496e7860acff5c48a10ae1de208
Bhargav-92 <tkr.bhargav92@gmail.com>
2024-07-24 18:26:35 +0530

    feat :added shad cn and front end

--- Frontend/src/Components/ComplaintChatBot/ComplaintChatBot.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ComplaintChatBot = () => {
-  return <div>ComplaintChatBot</div>;
-};
-
-export default ComplaintChatBot;
--- Frontend/src/layouts/AuthLayout.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function AuthLayout({children}) {
-    return (
-        <div>
-            {children}
-        </div>
-    )
-}
