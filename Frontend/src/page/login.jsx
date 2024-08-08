4bc0259555622369a0bbe43ae8786fbf3646979c
Bhargav-92 <tkr.bhargav92@gmail.com>
2024-08-08 18:25:56 +0530

    feat : added pages

--- Frontend/src/page/login.jsx
+++ Frontend/src/page/login.jsx
@@ -1,16 +1,10 @@
+import Navbar from '../component/Navbar';
+
 const Login = () => {
   return (
-    <details className="dropdown">
-      <summary className="btn m-1">open or close</summary>
-      <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-        <li>
-          <a>Item 1</a>
-        </li>
-        <li>
-          <a>Item 2</a>
-        </li>
-      </ul>
-    </details>
+    <>
+      <Navbar />
+    </>
   );
 };
 
