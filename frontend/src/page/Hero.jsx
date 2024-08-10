42bbfd5878cb253389f9a2f163828f821abb0cea
Bhargav-92 <tkr.bhargav92@gmail.com>
2024-08-10 17:44:16 +0530

    feat : added home image

--- frontend/src/page/Hero.jsx
+++ frontend/src/page/Hero.jsx
@@ -1,15 +1,34 @@
+import HomeSvg from '/homeSvg.png';
+
 const Hero = () => {
   return (
     <div className="hero bg-base-200 min-h-screen">
-      <div className="hero-content text-center">
-        <div className="max-w-md">
-          <h1 className="text-5xl font-bold">Hello there</h1>
-          <p className="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-          <button className="btn bg-[#F77B00] text-white">Get Started</button>
+      <div className="hero-content grid grid-cols-1 lg:grid-cols-3 gap-8 items-center">
+        <div className="lg:col-span-2">
+          <h1 className="text-6xl lg:text-8xl font-bold text-[#F77B00]">
+            Consumer
+          </h1>
+          <h5 className="py-4 text-6xl lg:text-8xl">Service</h5>
+          <div className="flex items-center mt-3">
+            <span className="text-[#F57C00] text-6xl md:text-6xl">|</span>
+            <div className="ml-4">
+              <p className="text-sm">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              </p>
+              <p className="text-sm">
+                Voluptate neque in inventore illo voluptatem vel consequuntur
+                officiis aliquid quo
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center lg:justify-end">
+          <img
+            src={HomeSvg}
+            alt="Hero image"
+            className="hidden lg:block"
+            height={'auto'}
+          />
         </div>
       </div>
     </div>
