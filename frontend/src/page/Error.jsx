6eb9183cb1289758649131f065fb6150f7032dc8
Bhargav-92 <tkr.bhargav92@gmail.com>
2024-08-10 16:22:24 +0530

    feat: added error page

--- frontend/src/page/Error.jsx
+++ frontend/src/page/Error.jsx
@@ -1,4 +1,8 @@
+import { useNavigate } from 'react-router-dom';
+
 const Error = () => {
+  const navigate = useNavigate();
+
   return (
     <section className="bg-white dark:bg-gray-900 ">
       <div className="container flex items-center min-h-screen px-6 py-12 mx-auto">
@@ -37,7 +41,10 @@ const Error = () => {
               <span>Go back</span>
             </button>
 
-            <button className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
+            <button
+              className="w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600"
+              onClick={() => navigate('/home')}
+            >
               Take me home
             </button>
           </div>
