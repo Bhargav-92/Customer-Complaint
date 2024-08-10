b8cd787df91a65546972c4e81c9009f4f1ee7b02
Bhargav-92 <tkr.bhargav92@gmail.com>
2024-08-10 16:23:27 +0530

    fix: change location for hero page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import { Routes } from 'react-router-dom';
-import Hero from './component/Hero';
+import Hero from './page/Hero';
 import Error from './page/Error';
 import Login from './page/Login';
 import { ThemeProvider } from './utils/ThemeProvider';
 import { Route } from 'react-router-dom';
 import Navbar from './component/Navbar';
+import Register from './page/Register';
 
 function App() {
   return (
@@ -12,8 +13,9 @@ function App() {
       <ThemeProvider>
         <Routes>
           <Route index element={<Login />} />
+          <Route path={'/register'} element={<Register />} />
           <Route paht="/" element={<Navbar />}>
-            <Route path="/home" element={<Hero />} />
+            <Route index path="/home" element={<Hero />} />
           </Route>
           <Route path="*" element={<Error />} />
         </Routes>
