2386ce5f6b42a3e77b93a4d6fd917bdf14236b6f
Bhargav-92 <tkr.bhargav92@gmail.com>
2024-08-12 10:27:09 +0530

    fix: routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,18 @@
-import { Routes } from "react-router-dom";
-import Hero from "./component/Hero";
-import Login from "./page/login";
-import { ThemeProvider } from "./utils/ThemeProvider";
-import { Route } from "react-router-dom";
-import Navbar from "./component/Navbar";
+import { Routes } from 'react-router-dom';
+import Hero from './component/Hero';
+import Login from './page/login';
+import { ThemeProvider } from './utils/ThemeProvider';
+import { Route } from 'react-router-dom';
+import Navbar from './component/Navbar';
 
 function App() {
   return (
     <>
       <ThemeProvider>
-        <Hero />
         <Routes>
-          <Route index element={<Login />} />
-          <Route path="/" element={<Navbar />}>
-            <Route path="/dashboard" element={<Hero />} />
+          <Route path="/" element={<Login />} />
+          <Route element={<Navbar />}>
+            <Route index path="/dashboard" element={<Hero />} />
           </Route>
         </Routes>
       </ThemeProvider>
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -8,6 +8,6 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <App />
-    </BrowserRouter>F
+    </BrowserRouter>
   </StrictMode>
 );
