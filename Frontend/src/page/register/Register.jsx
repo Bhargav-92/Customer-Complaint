b177ff8617da5aabc0cf211183eb021abee2329b
Bhargav-92 <tkr.bhargav92@gmail.com>
2024-08-17 15:08:46 +0530

    feat : created  register page

---
+++ Frontend/src/page/register/Register.jsx
@@ -0,0 +1,64 @@
+import loginImeg from '/public/login.png';
+
+const Login = () => {
+  return (
+    <>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content grid grid-cols-1 lg:grid-cols-2 gap-20">
+          <img
+            src={loginImeg}
+            className="max-w-md lg:max-w-lg rounded-lg shadow-2xl"
+          />
+          <div>
+            <h1 className="text-5xl font-bold">Login</h1>
+            <p className="py-6">Welcome to customer complaint system !!!</p>
+            <form className="space-y-4">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered w-full "
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered w-full "
+                  required
+                />
+                <div className="flex justify-between mt-5">
+                  <label className="label cursor-pointer gap-3">
+                    <input
+                      type="checkbox"
+                      defaultChecked
+                      className="checkbox"
+                    />
+                    <span className="label-text">Remember me</span>
+                  </label>
+                  <label className="label">
+                    <a href="#" className="label-text-alt link link-hover">
+                      Forgot password?
+                    </a>
+                  </label>
+                </div>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary w-full">Login</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
