b0a46f692cdee5c0fe0132211e9adbaef1547ebe
Bhargav-92 <tkr.bhargav92@gmail.com>
2024-08-20 16:12:18 +0530

    feat (admin ) : created admin main page

--- Frontend/src/component/admin/AdminMain.tsx
+++ Frontend/src/component/admin/AdminMain.tsx
@@ -1,9 +1,67 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { GoBellFill } from 'react-icons/go';
 
 const AdminMain = () => {
   return (
-    <div>
-      <h2>AdminMain</h2>
+    <div className="flex flex-col md:flex-col">
+      <section className="w-auto md:w-[70%] h-full">
+        <div className="navbar bg-base-100 ">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h7"
+                  />
+                </svg>
+              </div>
+            </div>
+          </div>
+          <div className="navbar-end gap-6">
+            <button className="btn btn-ghost btn-circle">
+              <div className="indicator">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                  />
+                </svg>
+                <span className="badge badge-xs badge-primary indicator-item"></span>
+              </div>
+            </button>
+            <div className="avatar">
+              <div className="ring-primary ring-offset-base-100 w-10 rounded-full ring ring-offset-2">
+                <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="w-full md:w-[30%]">
+        <Outlet />
+      </section>
     </div>
   );
 };
